ad8c3010272870587c69bb6a047f97a3e2adfcce
EdoKur <edok7179@gmail.com>
2021-12-19 20:33:59 +0700

    fitur forgot password aktif

---
+++ forgot_pass_two.php
@@ -0,0 +1,9 @@
+<form action="update_password_user.php" method="POST">
+<label  class="d-block input-label">Username</label>
+    <input type="text" name="username">
+<label  class="d-block input-label">New Password</label>
+    <input type="text" name="password">
+  <label  class="d-block input-label">Konfirmasi Password</label>
+    <input type="text" name="konfirmasi_password">
+    <input type="submit" name='submit'>
+</form>
