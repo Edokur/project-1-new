4e2f19cd435e4db6f3b10a9e05fdf9a37f4cef6a
EdoKur <edok7179@gmail.com>
2021-12-21 13:29:25 +0700

    memperbarui halam update produk dan title

--- Dasboard/update_produk_discount.php
+++ Dasboard/update_produk_discount.php
@@ -8,12 +8,13 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Update-Produk-Discount</title>
   </head>
   <body>
-  <a href="new_product.php" class="btn btn-primary">Kembali</a>
-    <h1>Update Data</h1>
-    <?php
+  <div class="container-fluid">
+
+<!-- Form Inputan -->
+<?php
       include "../conection_database.php";
       if (isset($_GET['id_produk'])){
         if($_GET['id_produk'] != ""){
@@ -27,43 +28,52 @@
       }else{
         header("location:discount_product.php");
       }
-?>
-
+  ?>
+  <div class="col bg-white p-5 rounded">
+    <a href="discount_product.php" class="btn btn-primary my-3">Kembali</a>
+    <h3 class="my-3">Update Data</h3>
     <form method="POST" action="proses_update_data_discount.php" enctype="multipart/form-data">
-      <input type="hidden" name="id_Produk" value="<?php echo $x['id_produk']; ?>">
+    <input type="hidden" name="id_Produk" value="<?php echo $x['id_produk']; ?>">
       <div class="mb-3">
-        <label  class="form-label">Kode Produk</label>
-        <input name="kode_Produk" type="text" class="form-control" id="nama_produk" aria-describedby="emailHelp" value="<?php echo $x['kode_produk']; ?>" >
+        <label for="exampleFormControlInput1" class="form-label">Kode Produk</label>
+        <input name="kode_Produk" type="text" class="form-control" id="nama_produk" aria-describedby="emailHelp" value="<?php echo $x['kode_produk']; ?>">
       </div>
       <div class="mb-3">
-        <label  class="form-label">Nama Produk</label>
-        <input name="nama_Produk" type="text" class="form-control" id="nama_produk" aria-describedby="emailHelp" value="<?php echo $x['nama_produk']; ?>" >
+        <label for="exampleFormControlInput1" class="form-label">Nama Produk</label>
+        <input name="nama_Produk" type="text" class="form-control" id="nama_produk" aria-describedby="emailHelp" value="<?php echo $x['nama_produk']; ?>">
       </div>
       <div class="mb-3">
-        <label class="form-label">Harga Produk</label>
-        <input name="harga_Produk" type="text" class="form-control" id="exampleInputPassword1" value="<?php echo $x['harga_produk']; ?>" >
+        <label for="exampleFormControlInput1" class="form-label">Harga Sebelum Diskon</label>
+        <input name="harga_awal" type="text" class="form-control" id="exampleInputPassword1" value="<?php echo $x['harga_awal']; ?>">
       </div>
       <div class="mb-3">
-        <label class="form-label">Diskon Produk</label>
-        <input name="diskon_Produk" type="text" class="form-control" id="exampleInputPassword1" value="<?php echo $x['diskon_produk']; ?>" >
+        <label for="exampleFormControlInput1" class="form-label">Harga Sesudah Diskon</label>
+        <input name="harga_Produk" type="text" class="form-control" id="exampleInputPassword1" value="<?php echo $x['harga_produk']; ?>">
       </div>
       <div class="mb-3">
-        <label  class="form-label">Stock Produk</label>
-        <input name="stock_Produk" type="text" class="form-control" id="stock_produk" aria-describedby="emailHelp" value="<?php echo $x['stock_produk']; ?>" >
+        <label for="exampleFormControlInput1" class="form-label">Diskon Produk</label>
+        <input name="diskon_produk" type="text" class="form-control" id="exampleInputPassword1" value="<?php echo $x['diskon_produk']; ?>">
       </div>
       <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">Deskripsi Produk</label>
-        <textarea name="detail_Produk" class="form-control" id="exampleFormControlTextarea1" rows="3" ><?php echo $x['detail_produk']; ?></textarea>
+        <label for="exampleFormControlInput1" class="form-label">Stock Produk</label>
+        <input name="stock_Produk" type="text" class="form-control" id="stock_produk" aria-describedby="emailHelp" value="<?php echo $x['stock_produk']; ?>">
       </div>
       <div class="mb-3">
-        <label  class="form-label">Gambar Produk</label>
-        <img src="gambar_diskon/<?php echo $x['foto_produk']; ?>" width="40" alt="">
-        <input class="form-control text-center" type="file" id="formFile" name="gambar_Produk" >
-        <p style="color: red">Ekstensi yang diperbolehkan .png | .jpg | .jpeg | .gif</p>
+        <label for="floatingTextarea2">Deskripsi Produk</label>
+        <textarea class="form-control" name="detail_Produk" id="floatingTextarea2" style="height: 200px"><?php echo $x['detail_produk']; ?></textarea>
       </div>
-      <button type="submit" class="btn btn-primary ">Update</button>
+      <div class="mb-3">
+        <label for="formFile" class="form-label">Gambar Produk</label>
+        <img src="gambar_diskon/<?php echo $x['foto_produk']; ?>" width="40" class="mb-2">
+        <input class="form-control text-center" type="file" id="formFile" name="gambar_Produk">
+        <p class="text-danger p-3">Ekstensi yang diperbolehkan .png | .jpg | .jpeg | .gif</p>
+      </div>
+      <div class="mb-3">
+        <button type="Submit" class="btn btn-primary my-3 text-center px-5">Update Data</button>
+      </div>
     </form>
-
+  </div>
+</div>
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
