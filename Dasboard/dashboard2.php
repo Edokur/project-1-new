04c75bbfa1904f3a6d84aac9319bf641b668078b
Ajibon-cmd <teguhbayu52@gmail.com>
2021-12-21 14:03:49 +0700

    update catalog dan menghapus sql

--- Dasboard/dashboard2.php
+++ Dasboard/dashboard2.php
@@ -70,7 +70,7 @@ include "../conection_database.php";
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="">
                 <!-- <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div> -->
@@ -82,7 +82,7 @@ include "../conection_database.php";
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-                <a class="nav-link " href="#">
+                <a class="nav-link " href="">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
